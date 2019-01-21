1eb2a723aa6d31cb4579355edabee73d7251cbe8
Daniel Like <41085135+dlike230@users.noreply.github.com>
2019-01-20 23:57:38 -0500

    intial commit

---
+++ generator.py
@@ -0,0 +1,38 @@
+import os
+
+from main import get_text_from_url
+
+
+def generate_file_from_url(url: str):
+    text = get_text_from_url(url)
+    if len(text) < 200:
+        return False
+    opened_file = open(find_free_filename("./generated/"), "w", encoding="utf-8")
+    opened_file.write(text)
+    opened_file.close()
+    return True
+
+
+def find_free_filename(directory, iteration=0):
+    desired_name = directory + "file_" + str(iteration) + ".txt"
+    if os.path.isfile(desired_name):
+        return find_free_filename(directory, iteration=iteration + 1)
+    return desired_name
+
+
+def get_apnotes_url(index: int):
+    return "https://apnotes.net/notes-12e/ch%d-12e.html" % index
+
+
+def generate_apnotes_files(count: int):
+    for i in range(1, count + 1):
+        url = get_apnotes_url(i)
+        print(url)
+        generate_file_from_url(url)
+
+
+def generate_wiki_files(count: int):
+    generated = 0
+    while generated < count:
+        if generate_file_from_url("https://en.wikipedia.org/wiki/Special:Random"):
+            generated += 1
---
+++ main.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import urllib.request
+from urllib.request import Request
+import functions
+
+
+def getInp(url):
+    req = Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36')
+    response = urllib.request.urlopen(req).read()
+    html = response.decode('utf-8', errors='ignore').strip()
+    html = html.replace("\\n", '\n').replace("\\'", "'").replace("\'", "'").replace("\\r", " ").replace("\\t", " ")
+    return html
+
+
+def get_text_from_url(url):
+    htext = getInp(url)
+    soup = BeautifulSoup(htext, "html.parser")
+    return functions.get_text(soup)
